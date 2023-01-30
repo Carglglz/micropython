9353a6531392e4b7e74c3c3996e30332a5b1dfe4
Carlosgg <carlosgilglez@gmail.com>
2023-01-30 18:08:26 +0000

    samd/boards: Add FATFS to MKRZERO.
    Signed-off-by: Carlosgg <carlosgilglez@gmail.com>

--- ports/samd/boards/ARDUINO_MKRZERO/mpconfigboard.h
+++ ports/samd/boards/ARDUINO_MKRZERO/mpconfigboard.h
@@ -1,4 +1,11 @@
 #define MICROPY_HW_BOARD_NAME "MKRZERO"
 #define MICROPY_HW_MCU_NAME   "SAMD21G18A"
 
-#define MICROPY_HW_DFLL_USB_SYNC    (1)
+#define MICROPY_HW_XOSC32K                      (1)
+#define MICROPY_PY_FSTRINGS         (1)
+
+// fatfs configuration used in ffconf.h
+#define MICROPY_FATFS_ENABLE_LFN            (1)
+#define MICROPY_FATFS_RPATH                 (2)
+#define MICROPY_FATFS_MAX_SS                (4096)
+#define MICROPY_FATFS_LFN_CODE_PAGE         437 /* 1=SFN/ANSI 437=LFN/U.S.(OEM) */
--- ports/samd/boards/ARDUINO_MKRZERO/mpconfigboard.mk
+++ ports/samd/boards/ARDUINO_MKRZERO/mpconfigboard.mk
@@ -1,8 +1,11 @@
 MCU_SERIES = SAMD21
 CMSIS_MCU = SAMD21G18A
-LD_FILES = boards/samd21x18a.ld sections.ld
+LD_FILES = boards/samd21x18a_fat.ld sections.ld
 TEXT0 = 0x2000
 
 # The ?='s allow overriding in mpconfigboard.mk.
 # MicroPython settings
 MICROPY_VFS_LFS1 ?= 1
+MICROPY_VFS_FAT ?= 1
+SRC_C += \
+	fatfs_port.c
